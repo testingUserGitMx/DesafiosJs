8b62c509229a03ca738f793568a080a2c957c857
rgzrgzm <mariomkt97@gmail.com>
2021-10-25 22:24:49 -0700

    Desafio#2

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 
-/* DESAFIO # 1  
+/* DESAFIO # 1  👇
 
-let nombre = prompt('Tu nombre')
-let edad = prompt('Tu edad')
+let nombre = prompt('Tu nombre');
+let edad = prompt('Tu edad');
 
-alert("Tu nombre es" + " " + nombre)
-alert("Tienes" + " " + edad + " " + "de edad")
+alert("Tu nombre es" + " " + nombre);
+alert("Tienes" + " " + edad + " " + "de edad");
 */
 
 
-// DESAFIO # 2
+// DESAFIO # 2 👇
 
-let number = Number(prompt('Ingrese un numero ganador ...'))
+/* 
+let number = Number(prompt('Ingrese un numero ganador ...'));
 
 if(number > 10 && number < 50) {
     alert('¡Felicidadez, Haz escogido un numero ganador 🎉!')
 } else {
     alert('Numero no ganador, suerte en la proxima...')
-}
+};
+
+*/
+
+
+ 
+ 
