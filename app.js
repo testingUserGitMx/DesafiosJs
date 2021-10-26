f67a6966da14053f0dce3ced51eac9c68ce9a502
rgzrgzm <mariomkt97@gmail.com>
2021-10-25 22:52:15 -0700

    Desafio#3

--- app.js
+++ app.js
@@ -22,6 +22,28 @@ if(number > 10 && number < 50) {
 
 */
 
+// DESAFIO # 3 👇
+
+/* 1️⃣ ALGORITMO QUE MULTIPLICA UN NUMERO 5 VECES 
+
+let ingresarNumero = Number(prompt('Ingrese un numero ...'));
+
+for(let i = 1; i <= 5; i++) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero + "x" + i + "=" + resultado)
+}
+
+*/
+
+/* 2️⃣ Pedir un texto mediante prompt, concatenar un valor en cada repetición, 
+realizando una salida por cada resultado hasta que se ingresa “ESC”. */
+ 
+ let texto = prompt('Escribe algo...');
+ 
+ while(texto != "ESC"){
+     alert('Bienvenido, la palabra magica es "ESC"');
+     texto = prompt('Palabra magica?')
+ }
 
  
  
