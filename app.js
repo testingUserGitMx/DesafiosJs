3e852fcab2a3329dc3c1dd8aa7dcba1a2e90677a
rgzrgzm <mariomkt97@gmail.com>
2021-10-25 20:22:43 -0700

    D#2

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
+
+/* DESAFIO # 1  
+
 let nombre = prompt('Tu nombre')
 let edad = prompt('Tu edad')
 
 alert("Tu nombre es" + " " + nombre)
 alert("Tienes" + " " + edad + " " + "de edad")
+*/
+
+
+// DESAFIO # 2
+
+let number = Number(prompt('Ingrese un numero ganador ...'))
+
+if(number > 10 && number < 50) {
+    alert('¡Felicidadez, Haz escogido un numero ganador 🎉!')
+} else {
+    alert('Numero no ganador, suerte en la proxima...')
+}
