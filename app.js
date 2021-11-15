60bd0e2045f7d865c5c7198365b6ebe6454557cb
rgzrgzm <mariomkt97@gmail.com>
2021-11-15 01:00:49 -0800

    Desafio#9

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const formulario = document.querySelector("#formulario");
 const alertSuccess = document.querySelector("#alertSuccess");
-const template = document.querySelector("#template").content;
+const inputNombre = document.getElementById("inputNombre");
+const inputEntradas = document.getElementById("inputEntradas");
+const hour = document.getElementById("hour");
+const btnAgregar = document.getElementById("btnAgregar");
+const ul = document.getElementById("ul")
 const d = document;
-
 
 class Ticket {
   constructor(pelicula, horario, entradas) {
@@ -11,38 +14,65 @@ class Ticket {
     this.entradas = entradas;
   }
 
-  comprarEntrada() {
-    document.write(`
-    <p> Haz comprado ${this.entradas} entrada(s) para la pelicula ${this.pelicula} 
-    a las ${this.horario} hrs</p>`);
+  // comprarEntrada() {
+  //   document.write(`
+  //   <p> Haz comprado ${this.entradas} entrada(s) para la pelicula ${this.pelicula}
+  //   a las ${this.horario} hrs</p>`);
 
-    entradasList.push(this.entradas);
-  }
+  //   entradasList.push(this.entradas);
+  // }
 
   verPelicula() {
     document.write(`Estas viendo: ${this.pelicula}`);
   }
 }
 
-const user = new Ticket("Infinity War", "6PM", 2);
-
+// ARRAY DE ENTRADAS
 const entradasList = [];
 
-user.comprarEntrada();
-user.verPelicula();
+// user.comprarEntrada();
+// user.verPelicula();
 
-console.log(entradasList);
-
-
-// BIENVENIDA DINAMICA h1
+// BIENVENIDA DINAMICA h2
 
 const bienvenida = (name) => {
-  d.querySelector("h2").innerText = `¡Bienvenid@ ${name} !`
-  
-}
+  d.querySelector("h2").innerText = `¡Bienvenid@ ${name} !`;
+};
 
+// RENDER
+// d.addEventListener("DOMContentLoaded", () => {
+//   let name = prompt("Login - Ingresa tu nombre:");
+//   bienvenida(name);
+// });
 
-d.addEventListener("DOMContentLoaded", (e) => {
-  let name = prompt("Login - Ingresa tu nombre:");
-  bienvenida(name);
-} )
+const agregarTicket = (userTicket) => {
+  entradasList.push(userTicket);
+};
+
+const mostrarTicket = () => {
+  entradasList.forEach((ticket) => {
+    
+    const { pelicula, horario, entradas } = ticket;
+    ul.innerHTML = 
+    `<li>Nombre de la pelicula: ${pelicula}</li>
+     <li>Horario: ${horario}</li>
+     <li>Entradas compradas: ${entradas}</li>`
+
+  });
+};
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const pelicula = inputNombre.value;
+  const entradas = inputEntradas.value;
+  const horario = hour.value;
+
+  const userTicket = new Ticket(pelicula, horario, entradas);
+
+  agregarTicket(userTicket);
+
+  console.log(entradasList);
+
+  mostrarTicket();
+});
