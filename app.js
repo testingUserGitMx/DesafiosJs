c4e22933ad7434a22421096452286e4566f517e8
rgzrgzm <mariomkt97@gmail.com>
2021-11-22 13:22:24 -0800

    updates & l.s.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const inputEntradas = document.getElementById("inputEntradas");
 const hour = document.getElementById("hour");
 const btnAgregar = document.getElementById("btnAgregar");
 const ul = document.getElementById("ul");
+const list = document.getElementById("list");
 const d = document;
 
 class Ticket {
@@ -14,50 +15,72 @@ class Ticket {
     this.entradas = entradas;
   }
 
-  // comprarEntrada() {
-  //   document.write(`
-  //   <p> Haz comprado ${this.entradas} entrada(s) para la pelicula ${this.pelicula}
-  //   a las ${this.horario} hrs</p>`);
-
-  //   entradasList.push(this.entradas);
-  // }
-
   verPelicula() {
     document.write(`Estas viendo: ${this.pelicula}`);
   }
 }
 
-
-// BIENVENIDA DINAMICA h2
-
+/* BIENVENIDA DINAMICA h2
 const bienvenida = (name) => {
   d.querySelector("h2").innerText = `¡Bienvenid@ ${name} !`;
 };
+*/
 
 // ARRAY DE ENTRADAS
-const entradasList = [];
+let entradasList = [];
 
-
-// FN AGREGAR TICKET 
+// FN AGREGAR TICKET
 const agregarTicket = (userTicket) => {
   entradasList.push(userTicket);
-  localStorage.setItem('tickets', JSON.stringify(userTicket))
+};
+
+const templateTicket = (ticket) => {
+  const { pelicula, horario, entradas } = ticket;
+
+  list.innerHTML += `
+  <div class="row alert alert-warning d-flex align-items-center" role="alert">
+
+    <div class="col-5"> 
+      Nombre de la pelicula: <b>${pelicula}</b>
+    </div>
+    <div class="col-3"> 
+      Horario: <b>${horario}</b> hrs.
+    </div>
+    <div class="col-2"> 
+      Entradas compradas: <b>${entradas}</b>
+    </div>
+    <div class="col-2 text-center"> 
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+      </svg>
+    </div>
+  
+  </div>
+  `;
 };
 
 // FN MOSTRAR TICKET
 const mostrarTicket = () => {
-  entradasList.forEach((ticket) => {
-    
-    const { pelicula, horario, entradas } = ticket;
-    
-    ul.innerHTML += `<li>Nombre de la pelicula: ${pelicula}</li>
-    <li>Horario: ${horario}</li>
-    <li>Entradas compradas: ${entradas}</li>`;
-  });
+  (list.innerHTML = ""),
+    entradasList.forEach((ticket) => {
+      templateTicket(ticket);
+    });
 };
 
+// FN PINTAR LOCALSTORAGE
+const pintarLS = () => {
+  (list.innerHTML = ""),
+    (entradasList = JSON.parse(localStorage.getItem("tickets")));
 
-
+  if (entradasList === null) {
+    entradasList = [];
+  } else {
+    entradasList.forEach((element) => {
+      templateTicket(element);
+    });
+  }
+};
 
 formulario.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -72,34 +95,12 @@ formulario.addEventListener("submit", (e) => {
 
   console.log(entradasList);
 
+  localStorage.setItem("tickets", JSON.stringify(entradasList));
   mostrarTicket();
 });
 
 // DOM LOAD
 d.addEventListener("DOMContentLoaded", () => {
-  if (!localStorage.getItem("names")) {
-    let name = prompt("Login - Ingresa tu nombre:");
-    localStorage.setItem("names", name);
-    bienvenida(name);
-  }
 
-  if (localStorage.getItem("names")) {
-    for (let i = 0; i < localStorage.length; i++) {
-      let names = localStorage.key(i);
-      d.querySelector("h2").innerText = `¡Bienvenid@ ${localStorage.getItem(names)}!`;
-    }
-  }
-
-  if(localStorage.getItem('tickets')){
-    entradasList = JSON.parse(localStorage.getItem('tickets'))
-    mostrarTicket();
-    
-  }
-
-
-
-
-
+  pintarLS();
 });
-
-
