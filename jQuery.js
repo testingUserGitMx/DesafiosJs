eb03c0dd2bd2a3c84ed044dc112355ed64fb3f8b
rgzrgzm <mariomkt97@gmail.com>
2021-12-01 11:44:09 -0800

    Desafio#13 - Animaciones Concatenadas

--- jQuery.js
+++ jQuery.js
@@ -4,3 +4,14 @@ $('#btn1').click(() => {
     $('#btn1').text("Mostrar/Ocultar")
     
 })
+
+$('#btnAgregar').click(() => {
+    $('#alert').remove()
+    
+    $('#container-form').append(
+        '<div id="alert" class="mt-4 text-center alert alert-success" role="alert"> ¡Compra realizada con exito!</div>');
+        
+    $('#alert').fadeIn('slow').delay(2000).fadeOut('slow');
+        
+        
+})
